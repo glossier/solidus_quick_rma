44f4ccf097c29ce30399583659a185808f5101a7
Braden <braden.douglass@gmail.com>
2016-10-03 08:27:45 -0400

    Move To A Debuggable Poltergeist Adapter
    Required to be able to start a snapshot instance for debugging issues
    that will arise from using Backbone to power some of the views and
    handle events on the return_authorization page.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,11 @@ require 'database_cleaner'
 require 'ffaker'
 require 'capybara/poltergeist'
 
-Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :poltergeist_debug do |app|
+  Capybara::Poltergeist::Driver.new(app, inspector: true)
+end
+
+Capybara.javascript_driver = :poltergeist_debug
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
